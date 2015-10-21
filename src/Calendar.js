e135ca1437ce2b56e609d50eab13ac35269c980e
youngerheart <435495400@qq.com>
2015-10-21 10:44:33 +0800

    添加TimePicker

--- src/Calendar.js
+++ src/Calendar.js
@@ -3,159 +3,158 @@ const createElement = require('./tools/element').create;
 
 const today = moment();
 
-function Calendar(that, callback) {
-  // 渲染header, 再渲染多个月份的日历
-  var {el, config} = that;
-  this.el = el;
-  this.lang = config.lang;
-  this.calNum = config.numberOfCalendars;
-  this.current = config.range ? moment(config.range[0]) : (config.date ? moment(config.date) : moment());
-  this.reload = callback;
-  this.draw();
-}
-
-Calendar.prototype.draw = function() {
-  moment.locale(this.lang);
-  // 清空之前的数据
-  this.month = [];
-  this.el.innerHTML = '';
-  this.current = this.current.subtract(Math.ceil(this.calNum / 2), 'month');
-  for(var i = 0; i < this.calNum; i++) {
-    this.current = this.current.date(1).add(1, 'month');
-    this.drawHeader(i);
-    this.drawMonth(i);
-  }
-}
-
-Calendar.prototype.drawHeader = function(i) {
-  var self = this;
-  this.header = createElement('div', 'drp-header');
-  this.title = createElement('div', 'drp-month', this.current.format('MMM YYYY'));
-  if(!i) {
-    var right = createElement('div', 'drp-right');
-    right.addEventListener('click', function() {
-      self.nextMonth();
-    });
-
-    var left = createElement('div', 'drp-left');
-    left.addEventListener('click', function() {
-      self.prevMonth();
-    });
-    this.header.appendChild(right);
-    this.header.appendChild(left);
+class Calendar{
+  constructor(that, callback) {
+    // 渲染header, 再渲染多个月份的日历
+    var {el, config} = that;
+    this.el = el;
+    this.calNum = config.numberOfCalendars;
+    this.current = config.range ? moment(config.range[0]) : (config.date ? moment(config.date) : moment());
+    this.reload = callback;
+    this.draw();
   }
 
-  this.header.appendChild(this.title);
-  
-  this.el.appendChild(this.header);
-  this.drawWeekDays();
-  this.title.innerHTML = this.current.format('MMM YYYY');
-}
-
-Calendar.prototype.drawMonth = function(num) {
-  this.month.push(createElement('div', 'drp-month'));
-  this.backFill();
-  this.currentMonth();
-  this.fowardFill();
-  this.el.appendChild(this.month[this.month.length - 1]);
-}
-
-Calendar.prototype.backFill = function() {
-  var clone = this.current.clone();
-  var dayOfWeek = clone.day();
-
-  if (!dayOfWeek) {
-    return;
+  draw() {
+    // 清空之前的数据
+    this.month = [];
+    this.el.innerHTML = '';
+    this.current = this.current.subtract(Math.ceil(this.calNum / 2), 'month');
+    for(var i = 0; i < this.calNum; i++) {
+      this.current = this.current.date(1).add(1, 'month');
+      this.drawHeader(i);
+      this.drawMonth(i);
+    }
   }
 
-  clone.subtract(dayOfWeek + 1, 'days');
+  drawHeader(i) {
+    var self = this;
+    this.header = createElement('div', 'drp-header');
+    this.title = createElement('div', 'drp-month', this.current.format('MMM YYYY'));
+    if(!i) {
+      var right = createElement('div', 'drp-right');
+      right.addEventListener('click', function() {
+        self.nextMonth();
+      });
 
-  for (var i = dayOfWeek; i > 0; i--) {
-    this.drawDay(clone.add(1,'days'));
+      var left = createElement('div', 'drp-left');
+      left.addEventListener('click', function() {
+        self.prevMonth();
+      });
+      this.header.appendChild(right);
+      this.header.appendChild(left);
+    }
+
+    this.header.appendChild(this.title);
+    
+    this.el.appendChild(this.header);
+    this.drawWeekDays();
+    this.title.innerHTML = this.current.format('MMM YYYY');
+  }
+
+  drawMonth(num) {
+    this.month.push(createElement('div', 'drp-month'));
+    this.backFill();
+    this.currentMonth();
+    this.fowardFill();
+    this.el.appendChild(this.month[this.month.length - 1]);
+  }
+
+  backFill() {
+    var clone = this.current.clone();
+    var dayOfWeek = clone.day();
+
+    if (!dayOfWeek) {
+      return;
+    }
+
+    clone.subtract(dayOfWeek + 1, 'days');
+
+    for (var i = dayOfWeek; i > 0; i--) {
+      this.drawDay(clone.add(1,'days'));
+    }
+  }
+
+  fowardFill() {
+    var clone = this.current.clone().add(1, 'months').subtract(1, 'days');
+    var dayOfWeek = clone.day();
+
+    if (dayOfWeek === 6) {
+      return;
+    }
+
+    for (var i = dayOfWeek; i < 6; i++) {
+      this.drawDay(clone.add(1, 'days'));
+    }
+  }
+
+  currentMonth() {
+    var clone = this.current.clone();
+
+    while (clone.month() === this.current.month()) {
+      this.drawDay(clone);
+      clone.add(1, 'days');
+    }
+  }
+
+  getWeek(day) {
+    if (!this.week || day.day() === 0) {
+      this.week = createElement('div', 'drp-week');
+      this.month[this.month.length - 1].appendChild(this.week);
+    }
+  }
+
+  drawDay(day) {
+    var self = this;
+    this.getWeek(day);
+
+    //Outer Day
+    var outer = createElement('div', this.getDayClass(day));
+
+    //Day Number
+    var number = createElement('div', 'drp-day-number', day.format('DD'));
+    number.setAttribute('date', day.format('YYYY-MM-DD'));
+    outer.appendChild(number);
+    this.week.appendChild(outer);
+  }
+
+  getDayClass(day) {
+    var classes = ['drp-day'];
+    if (day.month() !== this.current.month()) {
+      classes.push('drp-other');
+    } else if (today.isSame(day, 'day')) {
+      classes.push('drp-today');
+    }
+    return classes.join(' ');
+  }
+
+  drawWeekDays(el) {
+    var self = this;
+    this.weekDays = createElement('div', 'drp-week-days')
+    // 获取一个星期的每一天
+    var weekdays = [];
+    for(var i = 0; i < 7; i++) {
+      weekdays.push(moment().weekday(i - 1).format('ddd'));
+    }
+    weekdays.forEach(function(weekday) {
+      var day = createElement('span', 'drp-day', weekday);
+      self.weekDays.appendChild(day);
+    })
+    this.el.appendChild(this.weekDays);
+  }
+
+  nextMonth() {
+    this.current.subtract(Math.floor(this.calNum / 2) - 1, 'months');
+    this.next = true;
+    this.draw();
+    this.reload();
+  }
+
+  prevMonth() {
+    this.current.subtract(Math.floor(this.calNum / 2) + 1, 'months');
+    this.next = false;
+    this.draw();
+    this.reload();
   }
 }
-
-Calendar.prototype.fowardFill = function() {
-  var clone = this.current.clone().add(1, 'months').subtract(1, 'days');
-  var dayOfWeek = clone.day();
-
-  if (dayOfWeek === 6) {
-    return;
-  }
-
-  for (var i = dayOfWeek; i < 6; i++) {
-    this.drawDay(clone.add(1, 'days'));
-  }
-}
-
-Calendar.prototype.currentMonth = function() {
-  var clone = this.current.clone();
-
-  while (clone.month() === this.current.month()) {
-    this.drawDay(clone);
-    clone.add(1, 'days');
-  }
-}
-
-Calendar.prototype.getWeek = function(day) {
-  if (!this.week || day.day() === 0) {
-    this.week = createElement('div', 'drp-week');
-    this.month[this.month.length - 1].appendChild(this.week);
-  }
-}
-
-Calendar.prototype.drawDay = function(day) {
-  var self = this;
-  this.getWeek(day);
-
-  //Outer Day
-  var outer = createElement('div', this.getDayClass(day));
-
-  //Day Number
-  var number = createElement('div', 'drp-day-number', day.format('DD'));
-  number.setAttribute('date', day.format('YYYY-MM-DD'));
-  outer.appendChild(number);
-  this.week.appendChild(outer);
-}
-
-Calendar.prototype.getDayClass = function(day) {
-  var classes = ['drp-day'];
-  if (day.month() !== this.current.month()) {
-    classes.push('drp-other');
-  } else if (today.isSame(day, 'day')) {
-    classes.push('drp-today');
-  }
-  return classes.join(' ');
-}
-
-Calendar.prototype.drawWeekDays = function(el) {
-  var self = this;
-  this.weekDays = createElement('div', 'drp-week-days')
-  // 获取一个星期的每一天
-  var weekdays = [];
-  for(var i = 0; i < 7; i++) {
-    weekdays.push(moment().weekday(i - 1).format('ddd'));
-  }
-  weekdays.forEach(function(weekday) {
-    var day = createElement('span', 'drp-day', weekday);
-    self.weekDays.appendChild(day);
-  })
-  this.el.appendChild(this.weekDays);
-}
-
-Calendar.prototype.nextMonth = function() {
-  this.current.subtract(Math.floor(this.calNum / 2) - 1, 'months');
-  this.next = true;
-  this.draw();
-  this.reload();
-}
-
-Calendar.prototype.prevMonth = function() {
-  this.current.subtract(Math.floor(this.calNum / 2) + 1, 'months');
-  this.next = false;
-  this.draw();
-  this.reload();
-}
-
 
 module.exports = Calendar;
--- src/TimePicker.js
+++ src/TimePicker.js
@@ -1,3 +1,78 @@
-function TimePicker() {}
+const createElement = require('./tools/element').create;
+
+class TimePicker {
+  constructor(that) {
+    var {el, config, selectFunc} = that;
+    this.el = el;
+    this.config = config;
+    this.selectFunc = selectFunc;
+    this.hours = [];
+    this.minutes = [];
+    this.timeText = null;
+    this.secondTimeText = null;
+    this.init(that);
+  }
+
+  init(that) {
+    var type = this.config.type;
+    var timeBox = createElement('div', 'drp-time');
+    this.timeText = createElement('div', 'drp-time-text');
+    if(type === 'single') {
+      timeBox.appendChild(this.timeText);
+      timeBox.appendChild(this.createInput(true, that.date));
+      timeBox.appendChild(this.createInput(false, that.date));
+    } else if(type === 'range' || type === 'terminal') {
+      this.secondTimeText = createElement('div', 'drp-time-text');
+      timeBox.appendChild(this.insertItemBox(this.timeText, this.createInput(true, that.range.start, true), this.createInput(false, that.range.start, true)));
+      timeBox.appendChild(this.insertItemBox(this.secondTimeText, this.createInput(true, that.range.end, false), this.createInput(false, that.range.end, false)));
+    }
+    this.el.appendChild(timeBox);
+  };
+
+  insertItemBox(text, rangeStart, rangeEnd) {
+    var timeItemBox = createElement('div', 'drp-time-item');
+    timeItemBox.appendChild(text);
+    timeItemBox.appendChild(rangeStart);
+    timeItemBox.appendChild(rangeEnd);
+    return timeItemBox;
+  }
+
+
+  createInput(isHour, now, isStart) {
+    var el = createElement('input', 'drp-time-input');
+    var current = isHour ? now.hours() : now.minutes();
+    el.type = 'range';
+    el.min = 0;
+    el.max = isHour ? 23 : 59;
+    el.defaultValue = current;
+    this.setParams(isHour, now, isStart);
+    
+    el.addEventListener('change', (e) => {
+      now = isHour ? now.hours(e.target.value) : now.minutes(e.target.value);
+      this.setParams(isHour, now, isStart);
+      this.selectFunc(null);
+    });
+    return el;
+  };
+
+  setParams(isHour, now, isStart) {
+    var type = this.config.type;
+    if(type === 'single') {
+      this.timeText.innerHTML = now.format('LT');
+      if(isHour) this.hours[0] = now.format('HH');
+      else this.minutes[0] = now.format('mm');
+    } else if(type === 'range' || type === 'terminal') {
+      if(isStart) {
+        this.timeText.innerHTML = now.format('LT');
+        if(isHour) this.hours[0] = now.format('HH');
+        else this.minutes[0] = now.format('mm');
+      } else {
+        this.secondTimeText.innerHTML = now.format('LT');
+        if(isHour) this.hours[1] = now.format('HH');
+        else this.minutes[1] = now.format('mm');
+      }
+    }
+  };
+}
 
 module.exports = TimePicker;
