774ea056c5aad0c33910c427af1f59c7087e6e7e
pei-qiao.peng <ppq1991@gmail.com>
2015-12-11 11:27:28 +0800

    add gui♂chu

--- src/lang.js
+++ src/lang.js
@@ -1,38 +1,50 @@
 module.exports = {
-  'zh-cn': {
-    today: '今天',
-    yesterday: '昨天',
-    lastWeek: '最近一周',
-    lastMonth: '最近一月',
-    custom: '自定义',
-    day: '日',
-    week: '周',
-    month: '月',
-    select: '选择',
-    cancel: '取消'
-  },
-  'en': {
-    today: 'today',
-    yesterday: 'yesterday',
-    lastWeek: 'last week',
-    lastMonth: 'last month',
-    custom: 'custom',
-    day: 'day',
-    week: 'week',
-    month: 'month',
-    select: 'select',
-    cancel: 'cancel'
-  },
-  'ja': {
-    today: '今日',
-    yesterday: '昨日',
-    lastWeek: '一週間',
-    lastMonth: '一月間',
-    custom: 'カスタマ',
-    day: '日',
-    week: '週',
-    month: '月',
-    select: '選択',
-    cancel: 'キャンセル'
-  }
+    'zh-cn': {
+        today: '今天',
+        yesterday: '昨天',
+        lastWeek: '最近一周',
+        lastMonth: '最近一月',
+        custom: '自定义',
+        day: '日',
+        week: '周',
+        month: '月',
+        select: '选择',
+        cancel: '取消'
+    },
+    'en': {
+        today: 'today',
+        yesterday: 'yesterday',
+        lastWeek: 'last week',
+        lastMonth: 'last month',
+        custom: 'custom',
+        day: 'day',
+        week: 'week',
+        month: 'month',
+        select: 'select',
+        cancel: 'cancel'
+    },
+    'ja': {
+        today: '今日',
+        yesterday: '昨日',
+        lastWeek: '一週間',
+        lastMonth: '一月間',
+        custom: 'カスタマ',
+        day: '日',
+        week: '週',
+        month: '月',
+        select: '選択',
+        cancel: 'キャンセル'
+    },
+    'guichu': {
+        today: '昨日',
+        yesterday: '上周',
+        lastWeek: '今日',
+        lastMonth: '去年',
+        custom: '你猜',
+        day: '月',
+        week: '日',
+        month: '周',
+        select: '取消',
+        cancel: '确定'
+    }
 };
